217fbfd1f960fbb34dad0bf9dc561bf779a3706e
Stanislav Kozh <printks@yandex.ru>
2022-09-07 21:59:59 +0300

    Add NavHost and first screen

---
+++ app/src/main/java/com/codexample/notesappmvvm/screens/Start.kt
@@ -0,0 +1,10 @@
+package com.codexample.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Notes App Test")
+}
