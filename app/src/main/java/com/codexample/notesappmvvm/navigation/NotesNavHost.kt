bff401007daa9721a8ca7a0d7f2881843e4c44da
Stanislav Kozh <printks@yandex.ru>
2022-09-08 00:12:19 +0300

    Release between screen navigation

--- app/src/main/java/com/codexample/notesappmvvm/navigation/NotesNavHost.kt
+++ app/src/main/java/com/codexample/notesappmvvm/navigation/NotesNavHost.kt
@@ -4,10 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.codexample.notesappmvvm.screens.Add
-import com.codexample.notesappmvvm.screens.Main
-import com.codexample.notesappmvvm.screens.Note
-import com.codexample.notesappmvvm.screens.Start
+import com.codexample.notesappmvvm.screens.AddScreen
+import com.codexample.notesappmvvm.screens.MainScreen
+import com.codexample.notesappmvvm.screens.NoteScreen
+import com.codexample.notesappmvvm.screens.StartScreen
 
 sealed class NavRoute(val route: String) {
     object Start: NavRoute("start_screen")
@@ -21,9 +21,9 @@ fun NotesNavHost() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = NavRoute.Start.route) {
-        composable(NavRoute.Start.route) { Start(navController = navController) }
-        composable(NavRoute.Main.route) { Main(navController = navController) }
-        composable(NavRoute.Add.route) { Add(navController = navController) }
-        composable(NavRoute.Note.route) { Note(navController = navController) }
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
     }
 }
--- app/src/main/java/com/codexample/notesappmvvm/screens/Start.kt
+++ app/src/main/java/com/codexample/notesappmvvm/screens/Start.kt
@@ -1,10 +1,61 @@
 package com.codexample.notesappmvvm.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.codexample.notesappmvvm.navigation.NavRoute
+import com.codexample.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Start(navController: NavHostController) {
-    Text(text = "Notes App Test")
+fun StartScreen(navController: NavHostController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "What will we use?")
+            Button(
+                onClick = {
+                    navController.navigate(route = NavRoute.Main.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Room database")
+            }
+            Button(
+                onClick = {
+                    navController.navigate(route = NavRoute.Main.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Firebase database")
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrevStartScreen() {
+    NotesAppMVVMTheme {
+        StartScreen(navController = rememberNavController())
+    }
 }
